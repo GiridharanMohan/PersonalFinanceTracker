508fdefec79390c924db48327543389c15cae898
GiridharanMohan <giri28602@gmail.com>
2026-07-26 15:22:41 +0530

    added entity classes

--- src/main/java/com/dev/personalFinanceTracker/model/User.java
+++ src/main/java/com/dev/personalFinanceTracker/model/User.java
@@ -1,4 +1,29 @@
 package com.dev.personalFinanceTracker.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users", schema = "Personal_Finance_Tracker")
+@Data
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Email(message = "Not a valid email ID")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,20}$")
+    private String password;
+
+    @NotBlank(message = "please specify the currency")
+    private String currency;
 }
