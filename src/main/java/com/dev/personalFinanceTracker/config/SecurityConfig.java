964f420af03574596a6718a4b06663eb291ed2f8
GiridharanMohan <giri28602@gmail.com>
2026-08-04 17:23:27 +0530

    code changes

--- src/main/java/com/dev/personalFinanceTracker/config/SecurityConfig.java
+++ src/main/java/com/dev/personalFinanceTracker/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.dev.personalFinanceTracker.config;
 import com.dev.personalFinanceTracker.util.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,6 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter){
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/users/**")
                         .permitAll()
--- src/main/java/com/dev/personalFinanceTracker/service/AccountService.java
+++ src/main/java/com/dev/personalFinanceTracker/service/AccountService.java
@@ -1,25 +1,85 @@
 package com.dev.personalFinanceTracker.service;
 
 import com.dev.personalFinanceTracker.model.Account;
+import com.dev.personalFinanceTracker.model.User;
 import com.dev.personalFinanceTracker.model.dto.AccountRequestDto;
 import com.dev.personalFinanceTracker.model.dto.AccountResponseDto;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import com.dev.personalFinanceTracker.model.dto.ResponseDto;
+import com.dev.personalFinanceTracker.repository.AccountRepository;
+import com.dev.personalFinanceTracker.repository.UserRepository;
+import com.dev.personalFinanceTracker.util.Util;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Slf4j
 @Service
 public class AccountService {
-    public String createAccount(Account account) {
+
+    @Autowired
+    private Util util;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //currently 1 user can have 1 account
+    //but actual plan is to make 1 person can have multiple accounts.
+    public ResponseDto<String> createAccount(AccountRequestDto account) {
+        try {
+            User currentUser = util.getCurrentUser();
+            if(accountRepository.findByUserId(currentUser.getId()).isPresent())
+                throw new RuntimeException("Account already exists for the user");
+
+            Account responseEntity = new Account();
+            responseEntity.setUser(currentUser);
+            responseEntity.setName(account.getName());
+            responseEntity.setBalance(new BigDecimal(0));
+            accountRepository.save(responseEntity);
+            return new ResponseDto<>(true,"successfully saved account",null);
+        }catch (Exception e) {
+            log.error("Exception occurred while creating account - {}", e.getMessage());
+            return new ResponseDto<>(false, "Error creating the account", null);
+        }
     }
 
-    public AccountResponseDto showAccount(long id) {
+    public ResponseDto<AccountResponseDto> showAccount() {
+        User user = util.getCurrentUser();
+        Optional<Account> account = accountRepository.findByUserId(user.getId());
+
+        if (account.isEmpty())
+            return new ResponseDto<>(false, "No account is linked with the user", null);
+
+        Account myAccount = account.get();
+        AccountResponseDto responseEntity = new AccountResponseDto();
+        responseEntity.setId(myAccount.getId());
+        responseEntity.setName(myAccount.getName());
+        responseEntity.setEmail(myAccount.getUser().getEmail());
+        responseEntity.setBalance(myAccount.getBalance());
+        return new ResponseDto<>(true, "Successfully found the linked account", responseEntity);
     }
 
-    public String editAccount(AccountRequestDto accountRequestDto) {
-    }
-
-    public String deleteAccount(long id) {
+    public ResponseDto<AccountResponseDto> editAccount(AccountRequestDto accountRequestDto) {
         return null;
+    }
+
+    public ResponseDto<String> deleteAccount() {
+        String msg = "Account does not exists";
+        try {
+            User user = util.getCurrentUser();
+            Account account = accountRepository.findByUserId(user.getId())
+                    .orElseThrow(() -> new RuntimeException(msg));
+
+            accountRepository.deleteById(account.getId());
+            return new ResponseDto<>(true, "Account deleted successfully", null);
+        } catch (Exception e) {
+            log.error("Error occurred while deleting the account - {}", e.getMessage());
+            return new ResponseDto<>(false, msg, null);
+        }
     }
 }
