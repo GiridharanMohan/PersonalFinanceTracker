99299b2065a8acbc7493ff177c0576eabcb20e92
GiridharanMohan <giri28602@gmail.com>
2026-08-16 15:46:27 +0530

    remodeling category and transaction entities

--- src/main/java/com/dev/personalFinanceTracker/model/Category.java
+++ src/main/java/com/dev/personalFinanceTracker/model/Category.java
@@ -1,25 +1,6 @@
 package com.dev.personalFinanceTracker.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Table(name = "categories")
-@Data
-@NoArgsConstructor
-public class Category {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "category_id")
-    private Long id;
-
-    @NotBlank(message = "Name is required")
-    private String name;
-
-    //here type refers to income or expenditure
-    @NotBlank(message = "Please specify a type")
-    private TransactionType type;
+public enum Category {
+    FOOD,
+    Trip
 }
